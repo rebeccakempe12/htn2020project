c46381d4135f481df5a48affe8360975cef62edc
Kelsey379 <77125152+Kelsey379@users.noreply.github.com>
2021-01-16 17:35:54 -0500

    Add files via upload

--- popup.js
+++ popup.js
@@ -7,3 +7,18 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     }, false);
   }, false);
+
+  chrome.runtime.onInstalled.addListener(() => {
+    console.log('onInstalled...');
+    chrome.alarms.create('refresh', { periodInMinutes: 25 });
+  });
+
+  chrome.alarms.onAlarm.addListener((alarm) => {
+    console.log(alarm.name); // refresh
+    timerDone();
+  });
+
+  function timerDone() {
+    alert("Time's Up!"),
+    console.log("Time's Up!!");
+  }
