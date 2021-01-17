1965c3829bb8765a8f2695b97ec0fbbdd7207192
Lune <lunasun918@gmail.com>
2021-01-17 00:21:35 -0500

    SO HAPPY

--- Webpage/app.js
+++ Webpage/app.js
@@ -2,9 +2,11 @@ function greeting() {
     var greeting;
     var hour = new Date().getHours();
 
-    if (hour < 12) {
+    if (hour < 3) {
         greeting = "Good Morning!";
-    } else if (hour >= 12 && hour < 3) {
+    } else if (hour >= 3 && hour < 6) {
+    } else if (hour >= 6 && hour < 12) {
+    } else if (hour >= 12 && hour < 15) {
         greeting = "Good Afternoon!";
     } else {
         greeting = "Good Evening!";
